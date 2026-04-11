5481241439115328f9c2c40ad71126b7d98d4e54
Pratik <pratik24@email.com>
2026-04-12 02:19:47 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const images = [
+    "assets/images/bg1.jpg",
+    "assets/images/bg2.jpg",
+    "assets/images/bg3.jpg"
+];
+
+let index = 0;
+
+function changeBackground() {
+    document.querySelector(".hero").style.backgroundImage =
+        `url(${images[index]})`;
+    index = (index + 1) % images.length;
+}
+
+setInterval(changeBackground, 3000);
+changeBackground();
