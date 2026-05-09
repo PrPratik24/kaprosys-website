7fd99cd3f1e8c04e85931cf959d57772d4d78a0b
PrPratik24 <153280048+PrPratik24@users.noreply.github.com>
2026-05-10 01:39:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -17,3 +17,12 @@ function changeBackground() {
 setInterval(changeBackground, 4000);
 
 changeBackground();
+
+/* MOBILE MENU */
+
+const mobileMenu = document.getElementById("mobile-menu");
+const navLinks = document.getElementById("nav-links");
+
+mobileMenu.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+});
