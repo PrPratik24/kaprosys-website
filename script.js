481ded14ff7ea4a85b688615cf3efe350ddb2deb
PrPratik24 <153280048+PrPratik24@users.noreply.github.com>
2026-05-10 01:24:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+const images = [
+    "assets/images/bg1.jpg",
+    "assets/images/bg2.jpg",
+    "assets/images/bg3.jpg"
+];
+
+let index = 0;
+
+function changeBackground() {
+
+    document.querySelector(".hero").style.backgroundImage =
+        `url('${images[index]}')`;
+
+    index = (index + 1) % images.length;
+}
+
+setInterval(changeBackground, 4000);
+
+changeBackground();
